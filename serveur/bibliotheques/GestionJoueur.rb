14b98cbdba113774c225b1b4b7f49cb7abd839b9
Thomas BOY <thomas.boy@orange.fr>
2014-06-26 17:42:33 +0200

    Partie serveur du projet

---
+++ serveur/bibliotheques/GestionJoueur.rb
@@ -0,0 +1,186 @@
+$LOAD_PATH << File.dirname(__FILE__) + "/jeu"
+
+require 'conf'
+
+class GestionJoueur
+
+	attr_writer :transmission
+
+	def initialize(wsJoueur, instancePartie, instanceJoueur, instanceCoordinationClient)
+		@ws = wsJoueur
+		@partie = instancePartie
+		@joueur = instanceJoueur
+		@coord = instanceCoordinationClient
+		@transmission = nil
+	end
+
+	def preparationClient(pseudo)
+
+		@joueur.definirPseudo(pseudo)
+		@coord.transmissionPseudo()
+		
+		# On envoie une synthèse des personnes participant et les idéologies associées
+		partenaires = @partie.obtenirTableauPartenaires()
+		envoieDonnees("partenaires", partenaires)
+		
+		# On envoie au client les niveaux idéaux de ses jauges.
+		niveaux = @joueur.niveauxIdeals()
+		envoieDonnees("jaugesIdeales", niveaux)
+
+	end
+
+
+	def miseAJour
+
+		# Envoi du temps pour la synchronisation
+		envoieDonnees("temps", @partie.temps())
+				
+		# S'il existe un événement, on l'envoie
+		if (@partie.evenement != nil)
+			envoieDonnees("evenement", @partie.obtenirEvenement)
+		end
+
+		puts "MaJ globale :"+@joueur.numJoueur.to_s
+		
+		# Envoi des informations utiles à l'actualisation du client
+		envoieDonnees("listeTerritoires", {'liste' => @partie.territoiresPartenaires, 'synthese' => @joueur.syntheseTerritoire})
+		envoieDonnees("positions", @partie.positionsJoueurs)
+		envoieDonnees("pcases", @partie.presenceCases)
+		envoieDonnees("fonds", @joueur.fondsFinanciers)
+		envoieDonnees("jauges", @joueur.syntheseJauge)
+		# envoieDonnees("syntheseTerritoires", @joueur.syntheseTerritoire)
+
+	end
+
+	def actionJoueur(caseCourante)
+
+		# Recuperation du type d'instance de case
+		type = @partie.plateau.recupererTypeCase(caseCourante)
+		case type
+			when "caseTerritoire"
+				# Génération des opérations sur un territoire
+				listeId = @partie.genererIdOperationsProposees()
+				
+				# Transmission des identifiants d'opération possibles et attente d'un choix
+				idActionChoisie = envoieDonneesAvecReponse("operations", listeId, 30)
+				
+				# Vérification des données venant du client
+				if((idActionChoisie.to_i.integer?) && listeId.include?(idActionChoisie))
+					# Création de l'opération choisie
+					operation = Operation.new(idActionChoisie, @partie.joueurCourant.ideologie.numero)
+					
+					# Repercussion du choix
+					@partie.appliquerOperationTerritoire(operation, caseCourante.territoire)
+				else
+					operation = Operation.new(listeId[0], @partie.joueurCourant.ideologie.numero)
+					
+					# Repercussion du choix
+					@partie.appliquerOperationTerritoire(operation, caseCourante.territoire)
+				end
+
+						  
+			when "caseEvenement"
+				# On réclame une opération sur événement
+				operation = caseCourante.selectionnerOperation()
+				  
+				# On transmet l'opération à Partie pour son application aux joueurs concernés
+				@partie.appliquerOperationEvenement(operation)
+	
+			when "caseDepart" # Muet
+				
+		end
+	end
+
+	def tourDuJoueur
+
+		# DEBUG
+		puts @joueur.numJoueur.to_s+" joue"
+
+		# On attend le lance de des
+		attendreReponse(10)
+	
+		# On calcule les des
+		de1, de2 = @partie.plateau.lanceDes()
+		
+		# Envoi du résultat obtenu
+		envoieDonnees("des", [de1, de2])
+	
+		# On fait progresser le joueur des des qu'il vient de lancer
+		caseCourante, passageDepart = @partie.progression(de1+de2)
+
+		# Envoi de la nouvelle position
+		envoieDonnees("position", caseCourante.numCase)
+
+		# Récupération de la rétribution du joueur
+		gain = @partie.recupererGain(passageDepart)
+
+		# Renseignement du gain obtenu
+		envoieDonnees("gain", gain)
+
+		# Permet de faire les différentes actions sur la case courante
+		actionJoueur(caseCourante)
+		
+
+		# Déclarer le tour suivant
+		@partie.tourSuivant()
+		
+		# Réveiller les joueurs inactifs
+		@partie.reveilFinTour()
+		
+		# Vérifier que le temps n'est pas dépassé
+		@partie.temps()
+	end
+
+	def tourJoueur
+		while (@partie.estDemarree)
+
+			# Mettre à jour des données du joueur
+			miseAJour()
+
+			# Obtenir le joueur courant
+			jc = @partie.joueurCourant.numJoueur
+		
+			# Transmettre le joueur courant
+			envoieDonnees("joueurCourant", jc)
+		
+			# Gestion du tour
+			if (@joueur.numJoueur == jc)
+
+				# Appel de la fonction gérant le tour du joueur courant
+			  	tourDuJoueur()
+
+			else # jc != numeroJoueur
+				puts @joueur.numJoueur.to_s+" attend" # DEBUG
+				
+				# Attendre la fin du tour
+				@partie.attendreFinTour()
+			end
+		
+		end # Fin du while (partie.estDemarree)
+	end
+	
+	
+	def envoieDonnees(identifiantCommunication, donnees)
+		@transmission = nil
+		@ws.send(tojson(identifiantCommunication, donnees))
+	end
+	
+	def envoieDonneesAvecReponse(identifiantCommunication, donnees, delai)
+		@transmission = nil
+		@ws.send(tojson(identifiantCommunication, donnees, delai))
+		return attendreReponse(delai)
+	end
+
+	# Permet d'attendre une réponse pendant un temps donné
+	def attendreReponse(delai)
+		$mutexReception.synchronize {
+			$cvReception.wait($mutexReception, delai)
+		}
+		
+		return @transmission
+	end
+
+	def envoyerSignalDeconnexion(numeroJoueurDeconnecte)
+		envoieDonnees("deconnexion", numeroJoueurDeconnecte)
+	end
+end
---
+++ serveur/serveur.rb
@@ -0,0 +1,123 @@
+$LOAD_PATH << File.dirname(__FILE__) + "/bibliotheques"
+
+require "web_socket"
+require "thread"
+require 'CoordinationClient'
+require 'Partie'
+require 'GestionJoueur'
+require 'conf'
+
+Thread.abort_on_exception = true
+
+if ARGV.size != 2
+	$stderr.puts("Usage: ruby sample/chat_server.rb ACCEPTED_DOMAIN PORT")
+	exit(1)
+end
+
+server = WebSocketServer.new(
+  :accepted_domains => [ARGV[0]], 
+  :port => ARGV[1].to_i())
+  
+puts("Server is running at port %d" % server.port)
+
+coord = CoordinationClient.new
+
+sem = Mutex.new
+
+server.run() do |ws| # ecoute des connexions
+	begin # Code du thread joueur
+		puts "connexion acceptee"
+		ws.handshake()
+		
+		# On initialise nos mutex/cv pour les communications
+		$mutexReception = Mutex.new
+		$cvReception = ConditionVariable.new
+		
+		# On recupère notre numero de joueur (en reveillant les autres thread si la partie peut commencer)
+		numJoueur = coord.nouveauJoueur()
+		
+		# Recuperation du pseudo
+		pseudo = ws.receive()
+		puts "Pseudo client = "+pseudo
+		
+		if (numJoueur < 0)
+			# La partie est pleine
+			ws.send(tojson("etat", -1))
+			
+			# On ferme la socket
+			ws.close()
+		else
+			sem.synchronize{
+				# Attente debut de partie
+				coord.attendreDebutPartie()
+				ws.send(tojson("numeroJoueur", numJoueur))
+				puts numJoueur.to_s+" est reveille"
+			}
+		
+			# Obtention de l'instance de Partie
+			partie = coord.obtenirPartie()
+			
+			# Instanciation du joueur
+			joueur = partie.recupererInstanceJoueur(numJoueur)
+
+			gestionJoueur = GestionJoueur.new(ws, partie, joueur, coord)
+			joueur.instanceGestionJoueur = gestionJoueur
+
+			gestionJoueur.preparationClient(pseudo)
+
+			semaphore = Mutex.new
+			
+			pingPrecedent = Time.now.to_i
+
+			# On ping le client toutes les X secondes pour vérifier sa présence
+			ping = Thread.new do
+				while partie.estDemarree
+					sleep($INTERVALLE_PING)
+					pingPrecedent = Time.now.to_i
+					ws.send("ping")
+				end
+			end
+
+			threadGestionJoueur = Thread.new do
+				gestionJoueur.tourJoueur()
+			end
+			
+			# Gestion des communication : filtre les réponses au ping et les transmissions utiles
+			communications = Thread.new do
+				while partie.estDemarree
+					transmission = ws.receive()
+			
+					if (transmission != "pong")
+						gestionJoueur.transmission = transmission
+						$mutexReception.synchronize {
+							$cvReception.signal
+						}
+					elsif (Time.now.to_i-pingPrecedent > $REPONSE_PING)
+						# On considère qu'un client ne répondant pas dans les temps est un joueur déconnecté
+						partie.deconnexionJoueur(numJoueur)
+				  	end
+				end
+			end
+			
+			# Endormir le thread principal (1 par joueur) tant que la partie est démarrée
+			partie.endormirFinPartie()
+
+			ping.kill()
+			communications.kill()
+
+			# Fin de la partie
+			sem.synchronize{ # Le premier accès se fait en écriture
+				scores = partie.obtenirScores()
+				ws.send(tojson("scores", scores))
+			}
+
+			sem.synchronize{
+				coord.nouvellePartie()
+			}
+		end
+		
+
+		
+		ws.close()
+	end
+end	
